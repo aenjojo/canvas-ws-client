98c844a822a97264186b1430e3220b9f8a414198
aenjojo <josuafernando999@gmail.com>
2025-03-16 02:15:56 +0700

    init canvas-ws-client

---
+++ README.md
@@ -0,0 +1,3 @@
+# canvas-ws: the client
+
+client implementation of moving object on canvas with of real-time movement update from server using websocket (uses TCP). here the client receive the latest updated of object position then update the canvas to redraw the box in the position received.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let canvas: HTMLCanvasElement;
+    let ctx: CanvasRenderingContext2D;
+    let socket;
+
+    const squareSize = 20;
+
+    function setupWebSocket() {
+        socket = new WebSocket("ws://localhost:3312/ws");
+
+        socket.addEventListener("open", () => {
+            console.log("WebSocket connected");
+        });
+
+        socket.addEventListener("message", (event) => {
+            const [x, y] = event.data.split(";").map(Number) as [
+                number,
+                number,
+            ];
+            if (!isNaN(x) && !isNaN(y)) {
+                drawSquare(x, y);
+            }
+        });
+
+        socket.addEventListener("close", () => {
+            console.log("WebSocket disconnected");
+            setTimeout(setupWebSocket, 1000);
+        });
+
+        socket.addEventListener("error", (error) => {
+            console.error("WebSocket error:", error);
+        });
+    }
+
+    function drawSquare(x: number, y: number) {
+        if (ctx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = "red";
+            ctx.fillRect(x, y, squareSize, squareSize);
+        }
+    }
+
+    onMount(() => {
+        if (canvas) {
+            ctx = canvas.getContext("2d")!;
+            setupWebSocket();
+        }
+    });
+</script>
+
+<main>
+    <canvas
+        bind:this={canvas}
+        width={500}
+        height={300}
+        style="border: 1px solid #ccc"
+    ></canvas>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
